473e5ed58e7037f4894d6e694eb20e439a34fa7f
alex_man <alexmnsbs@gmail.com>
2023-07-16 11:18:13 +0300

    feat/add binder

--- src/main/java/com/example/test/MainView.java
+++ src/main/java/com/example/test/MainView.java
@@ -1,11 +1,15 @@
 package com.example.test;
 
+import com.example.test.model.Item;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.data.converter.StringToIntegerConverter;
 import com.vaadin.flow.router.Route;
 
 @Route("main")
@@ -14,20 +18,55 @@ public class MainView extends VerticalLayout {
     private int textValue = 0;
 
     public MainView() {
-        TextField textField = new TextField();
-        textField.setValue(String.valueOf(textValue));
-        Button addButton = new Button("Change");
-        textField.addValueChangeListener(change->{
-            textValue = Integer.parseInt(textField.getValue());
+        TextField ftextField = new TextField();
+        ftextField.setValue(String.valueOf(textValue));
+        Button faddButton = new Button("Change");
+        ftextField.addValueChangeListener(change->{
+            textValue = Integer.parseInt(ftextField.getValue());
         });
-        addButton.addClickListener(click -> {
+        faddButton.addClickListener(click -> {
             textValue++;
-            textField.setValue(String.valueOf(textValue));
+            ftextField.setValue(String.valueOf(textValue));
         });
-        addButton.addClickShortcut(Key.ENTER);
+        faddButton.addClickShortcut(Key.ENTER);
+
+        Binder<Item> binder = new Binder<>(Item.class);
+
+        TextField textField = new TextField();
+        Button addButton = new Button("Change");
+        Item item = new Item(0);
+
+        binder.forField(textField)
+                .withConverter(
+                        new StringToIntegerConverter("Вводить нужно только целые числа"))
+                .bind(Item::getValue, Item::setValue);
+
+        binder.readBean(item);
+
+        addButton.addClickListener(e -> {
+            if (binder.validate().isOk()) {
+                item.setValue(item.getValue() + 1);
+                binder.readBean(item);
+            }
+        });
+
+        textField.addValueChangeListener(change->{
+            if (binder.validate().isOk()) {
+//                item.setValue(Integer.parseInt(textField.getValue()));
+                try {
+                    binder.writeBean(item);
+                } catch (ValidationException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         add(
                 new H1("Тестовое задание"),
+                new HorizontalLayout(
+                        ftextField,
+                        faddButton
+                ),
                 new HorizontalLayout(
                         textField,
                         addButton
---
+++ src/main/java/com/example/test/model/Item.java
@@ -0,0 +1,20 @@
+package com.example.test.model;
+
+public class Item {
+
+    private Integer value;
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public Item() {
+    }
+    public Item(Integer value) {
+        this.value = value;
+    }
+}
