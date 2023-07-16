ba8a3dd20efc6005b8de2f675be39ae062b91f28
alex_man <alexmnsbs@gmail.com>
2023-07-16 10:10:47 +0300

    feat/add button

--- src/main/java/com/example/test/MainView.java
+++ src/main/java/com/example/test/MainView.java
@@ -1,13 +1,37 @@
 package com.example.test;
 
-import com.vaadin.flow.component.html.NativeLabel;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 @Route("main")
 public class MainView extends VerticalLayout {
 
+    private int textValue = 0;
+
     public MainView() {
-        add(new NativeLabel("Your code here"));
+        TextField textField = new TextField();
+        textField.setValue(String.valueOf(textValue));
+        Button addButton = new Button("Change");
+        textField.addValueChangeListener(change->{
+            textValue = Integer.parseInt(textField.getValue());
+        });
+        addButton.addClickListener(click -> {
+            textValue++;
+            textField.setValue(String.valueOf(textValue));
+        });
+        addButton.addClickShortcut(Key.ENTER);
+
+        add(
+                new H1("Тестовое задание"),
+                new HorizontalLayout(
+                        textField,
+                        addButton
+                )
+        );
     }
 }
